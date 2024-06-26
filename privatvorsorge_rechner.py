5c3511600e1ee353c78aadd5e235a3c6093a9831
Kendrick Gruenberg <kendrick.gruenberg@t-online.de>
2024-06-27 00:16:38 +0200

    Brutto-Berechnung Vermögens-verzehrende MSCI-Verrentung (monatliche Entnahme)

--- privatvorsorge_rechner.py
+++ privatvorsorge_rechner.py
@@ -36,8 +36,8 @@ class colors: # https://stackoverflow.com/questions/287871/how-do-i-print-colore
 	CBEIGE2  = '\33[96m'
 	CWHITE2  = '\33[97m'
 
-def funktion_invertieren(funktion, funktionswert, name_zu_bestimmender_parameter, uebrige_parameter, praezision=0.01):
-	param_estimate = 1
+def funktion_invertieren(funktion, funktionswert, name_zu_bestimmender_parameter, uebrige_parameter, praezision=0.01, initialer_schaetzwert=1):
+	param_estimate = initialer_schaetzwert
 	step_size = 0.01
 	last_estimate_was_overestimate = None
 	while True: # Solange der Fehler über der gewünschten Präzision liegt (s.u.):
@@ -94,9 +94,12 @@ print(f"Es wird für sämtliche Berechnungen eine jährliche Inflation in Höhe 
 if inflations_anpassung in ["j", "J"]:
 	monatliche_wunschrente = monatliche_wunschrente * ( (1.0 + angenommene_inflationsrate_in_prozent/100.0)**dauer_sparphase_in_jahren )
 	print(f"Es wird nun eine inflationsangepasste Wunschrente von {monatliche_wunschrente} Euro/Monat für das Jahr {renteneintritts_jahr} angenommen.")
-	# Berechne und zeige außerdem wie hoch die Rente inflationsangepasst zum Renten-ENDE(!) sein müsste:
-	noetige_inflationsangepasste_rente_zum_rentenende = monatliche_wunschrente * ( (1.0 + angenommene_inflationsrate_in_prozent/100.0)**dauer_rentenphase_in_jahren ) # Achtung(!): monatliche_wunschrente wurde soeben bereits an die Inflation in der Sparphase angepasst!!!
-	print(f"{dauer_rentenphase_in_jahren} Jahre später, zum Rentenende mit {lebenserwartung} im Jahre {geburts_jahr+lebenserwartung} müsste die Rente inflationsangepasst {noetige_inflationsangepasste_rente_zum_rentenende} Euro/Monat betragen (im Fall, dass die Rente in der Rentenphase mit der Inflation steigen soll).")
+else:
+	print(f"Es wird eine Wunschrente von {monatliche_wunschrente} Euro/Monat für das Jahr {renteneintritts_jahr} angenommen.")
+
+# Berechne und zeige außerdem wie hoch die Rente inflationsangepasst zum Renten-ENDE(!) sein müsste:
+noetige_inflationsangepasste_rente_zum_rentenende = monatliche_wunschrente * ( (1.0 + angenommene_inflationsrate_in_prozent/100.0)**dauer_rentenphase_in_jahren ) # Achtung(!): monatliche_wunschrente wurde soeben bereits an die Inflation in der Sparphase angepasst!!!
+print(f"{dauer_rentenphase_in_jahren} Jahre später, zum Rentenende mit {lebenserwartung} im Jahre {geburts_jahr+lebenserwartung} müsste die Rente inflationsangepasst {noetige_inflationsangepasste_rente_zum_rentenende} Euro/Monat betragen (im Fall, dass die Rente in der Rentenphase mit der Inflation steigen soll).")
 
 jaehrliche_wunschrente = 12 * monatliche_wunschrente
 
@@ -244,20 +247,155 @@ print(f"(-) Es besteht eine Unsicherheit, wie im Jahr {renteneintritts_jahr} Kap
 # ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
 # ##### ##### ##### Vermögens-verzehrende Verrentungen: ##### ##### #####
 # ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
+
+# Berechnungen Entnahmepläne:
+
+angenommene_jaehrliche_rendite_entnahmeplan_in_prozent = 3.0 # (vgl. Handelsblatt vom 21.06.2024)
+
+def vermoegens_verzehrung_konstante_entnahme(vermoegen, monatliche_entnahme):
+	"""
+	Gibt die Dauer (in Jahren) zurück, die das anegegbene Vermögen {vermoegen} bei einer (konstanten) monatlichen Entnhame von {monatliche_entnahme} hält.
+	Annahme: Das Vermögen liegt ohne jegliche Verzinsung auf einem Konto.
+	"""
+	return vermoegen / (monatliche_entnahme * 12)
+
+def vermoegens_verzehrung_steigende_entnahme(vermoegen, monatliche_entnahme, jaehrliche_steigerung_entnahme):
+	"""
+	Gibt die Dauer (in Jahren) zurück, die das anegegbene Vermögen {vermoegen} bei einer monatlichen Entnhame von {monatliche_entnahme} hält,
+	welche allerdings jedes Jahr um {jaehrliche_steigerung_entnahme} steigt (um die Inflation auszugleichen).
+	Annahme: Das Vermögen liegt ohne jegliche Verzinsung auf einem Konto.
+	"""
+	uebriges_vermoegen = vermoegen
+	vergangene_monate = 0
+	aktuelle_monatliche_entnahme = monatliche_entnahme
+	while uebriges_vermoegen >= aktuelle_monatliche_entnahme:
+		uebriges_vermoegen -= aktuelle_monatliche_entnahme
+		vergangene_monate += 1
+		if vergangene_monate % 12 == 0: # nachdem 12 Monate vergangen sind / vor Beginn eines neuen Jahres:
+			aktuelle_monatliche_entnahme *= (1.0 + jaehrliche_steigerung_entnahme/100.0)
+	return vergangene_monate / 12.0
+
+def vermoegens_verzehrung_verzinster_entnahmeplan_konstante_entnahme(vermoegen, monatliche_entnahme, jaehrliche_rendite_entnahmeplan=angenommene_jaehrliche_rendite_entnahmeplan_in_prozent):
+	"""
+	Gibt die Dauer (in Jahren) zurück, die das anegegbene Vermögen {vermoegen} bei einer (konstanten) monatlichen Entnhame von {monatliche_entnahme} hält.
+	Annahme: Das Vermögen liegt in einem verzinsten Entnahmeplan (z.B. aus Anleihen/Festgeld) mit einer Verzinsung von {jaehrliche_rendite_entnahmeplan} Prozent.
+	Achtung: Steigt das Vermögen schneller als es entnommen wird, gibt diese Funktion Unendlich zurück!!!
+	"""
+	return vermoegens_verzehrung_verzinster_entnahmeplan_steigende_entnahme(vermoegen=vermoegen, monatliche_entnahme=monatliche_entnahme, jaehrliche_steigerung_entnahme=0.0, jaehrliche_rendite_entnahmeplan=jaehrliche_rendite_entnahmeplan)
+
+def vermoegens_verzehrung_verzinster_entnahmeplan_steigende_entnahme(vermoegen, monatliche_entnahme, jaehrliche_steigerung_entnahme, jaehrliche_rendite_entnahmeplan=angenommene_jaehrliche_rendite_entnahmeplan_in_prozent):
+	"""
+	Gibt die Dauer (in Jahren) zurück, die das anegegbene Vermögen {vermoegen} bei einer monatlichen Entnhame von {monatliche_entnahme} hält,
+	welche allerdings jedes Jahr um {jaehrliche_steigerung_entnahme} steigt (um die Inflation auszugleichen).
+	Annahme: Das Vermögen liegt in einem verzinsten Entnahmeplan (z.B. aus Anleihen/Festgeld) mit einer Verzinsung von {jaehrliche_rendite_entnahmeplan} Prozent.
+	Achtung: Steigt das Vermögen schneller als es entnommen wird, gibt diese Funktion Unendlich zurück!!!
+	"""
+	monatliche_rendite_entnahmeplan = 100.0 * ((1.0 + jaehrliche_rendite_entnahmeplan/100.0)**(1.0/12.0) - 1.0) # = 0,2466269772% bei 3,0% p.a. # = etwas weniger als 3,0% / 12,0 = 0,25%
+	uebriges_vermoegen = vermoegen
+	vergangene_monate = 0
+	aktuelle_monatliche_entnahme = monatliche_entnahme
+	while uebriges_vermoegen >= aktuelle_monatliche_entnahme:
+		uebriges_vermoegen_alt = uebriges_vermoegen
+		uebriges_vermoegen -= aktuelle_monatliche_entnahme # Das übrige Vermögen verringert sich jeden Monat um den monatlichen Entnahmebetrag...
+		uebriges_vermoegen *= (1.0 + monatliche_rendite_entnahmeplan/100.0) # ...da sich das Vermögen allerdings auch in einem verzinsten Entnahmeplan befindet, so erhöht sich das übrige Vermögen auch jeden Monat wieder etwas. # (lasse das Vermögen sich erst nach der 1. Entnahme vermehren!)
+		if uebriges_vermoegen > uebriges_vermoegen_alt: # Das Vermögen ist so gut verzinst, dass es durch die regelmäßigen Entnahmen gar nicht sinkt, sondern steigt...
+			return float("inf") # ...gebe Unendlich zurück, da das Vermögen so unendlich lange "hält"!
+		vergangene_monate += 1
+		if vergangene_monate % 12 == 0: # nachdem 12 Monate vergangen sind / vor Beginn eines neuen Jahres:
+			aktuelle_monatliche_entnahme *= (1.0 + jaehrliche_steigerung_entnahme/100.0)
+	return vergangene_monate / 12.0
+
 print("")
 print(colors.CGREEN + "##### ##### ##### Vermögens-verzehrende Verrentungen: ##### ##### #####" + colors.CEND)
 
 # ##### Besparen des MSCI World, monatliche Entnahme der Rente, Verzehrung des Vermögens: #####
 print("")
 print(colors.CGREEN + "##### Besparen des MSCI World, monatliche Entnahme der Rente, Verzehrung des Vermögens: #####" + colors.CEND)
-print("ToDo")
+print(f"Nehmen wir nun erneut an, dass du mit deinen monatlichen Sparbeiträgen einen ETF auf den MSCI World (oder einen vergleichbaren Fond) besparst (Sparplan).")
+print(f"Nehmen wir ferner erneut an, dass der MSCI World eine jährliche Rendite von {msci_world_rendite}% abwirft.")
+print(f"Schließlich nehmen wir an, dass du in deiner Rentenphase jeden Monat deine Rente in Höhe von {monatliche_wunschrente} Euro (brutto/netto je nach Betrachtung, s.u.) durch den Verkauf von Anteilen entnimmst " +\
+	f"und dass sich dein angespartes Vermögen dadurch allmählich verringert, also verzehrt(!) wird, bis es schließlich im Alter von {lebenserwartung} Jahren aufgebraucht ist.")
+print(f"Wir nehmen entweder an, dass (a) kein Inflationsausgleich in der Rentenphase stattfinden soll, dass die in der Rentenphase entnommene monatliche Rente also konstant bleibt und im Fall (b) dass sich deine monatlich entnomme Rente jedes Jahr um {angenommene_inflationsrate_in_prozent}% als Inflationsausgleich erhöht.")
+print(f"Ferner gehen wir entweder davon aus, dass du keinen separaten Entnahmeplan durchführst, die Rente also direkt durch regelmäßige Verkäufe deiner ETF-Anteile realisierst.")
+
+# (a) Ohne Inflationsausgleich (d.h. konstante Entnahme in der Rentenphase):
+
+print("")
+print("(a) Es soll kein(!) Inflationsausgleich in der Rentenphase stattfinden, die in der Rentenphase entnommene monatliche Rente bleibt konstant:")
+print("")
+print(f"(0) Monatliche Rente in Höhe von {monatliche_wunschrente} Euro ({colors.CGREEN}brutto{colors.CEND}):")
+
+# 1. Frage: Wie hoch muss mein angespartes Vermögen zum Renteneintritt sein, um daraus meine Wunschrente verzehren zu können?:
+notwendiges_vermoegen_fuer_msci_verzehrende_monatliche_entnahme_strategie = funktion_invertieren(vermoegens_verzehrung_verzinster_entnahmeplan_konstante_entnahme, dauer_rentenphase_in_jahren, "vermoegen", {"monatliche_entnahme": monatliche_wunschrente, "jaehrliche_rendite_entnahmeplan": msci_world_rendite}, praezision=0.1, initialer_schaetzwert=10000)
+
+print(f"Zum Renteneintritt musst du ein Vermögen von {notwendiges_vermoegen_fuer_msci_verzehrende_monatliche_entnahme_strategie} Euro im MSCI World angespart haben, um danach deine (mit der Inflation NICHT mitwachsende konstante) Rente in Höhe von {monatliche_wunschrente} Euro (brutto) verzehrend entnehmen zu können.")
+
+# 2. Frage: Wie hoch muss meine monatliche Sparrate (konstant oder mit Inflation wachsend) sein, um am Ende der Ansparphase das in 1. errechnete Vermögen angespart zu haben?:
+notwendige_konstante_monatliche_sparrate_fuer_msci_verzehrende_monatliche_entnahme_strategie = (1.0/12.0) * funktion_invertieren(sparplan_ausfuehren, notwendiges_vermoegen_fuer_msci_verzehrende_monatliche_entnahme_strategie, "sparrate_initial", {"sparrate_steigerungsrate": 1.00, "wertsteigerung_anlageobjekt": 1.00 + (msci_world_rendite/100.0), "anlage_dauer": dauer_sparphase_in_jahren})
+notwendige_wachsende_monatliche_sparrate_fuer_msci_verzehrende_monatliche_entnahme_strategie = (1.0/12.0) * funktion_invertieren(sparplan_ausfuehren, notwendiges_vermoegen_fuer_msci_verzehrende_monatliche_entnahme_strategie, "sparrate_initial", {"sparrate_steigerungsrate": 1.00 + (angenommene_inflationsrate_in_prozent/100.0), "wertsteigerung_anlageobjekt": 1.00 + (msci_world_rendite/100.0), "anlage_dauer": dauer_sparphase_in_jahren})
+
+print(f"=> Nötige monatliche Sparrate (konstant) = {colors.CGREEN}{notwendige_konstante_monatliche_sparrate_fuer_msci_verzehrende_monatliche_entnahme_strategie} Euro{colors.CEND}")
+print(f"=> Nötige monatliche Sparrate (muss jedes Jahr um {angenommene_inflationsrate_in_prozent}% erhöht werden) = {colors.CGREEN}{notwendige_wachsende_monatliche_sparrate_fuer_msci_verzehrende_monatliche_entnahme_strategie} Euro{colors.CEND}")
+print("")
+print(f"(1) Monatliche Rente in Höhe von {monatliche_wunschrente} Euro ({colors.CGREEN}netto{colors.CEND}, {kapitalertragssteuer_in_prozent}% Kapitalertragssteuer, {freibetrag_in_euro} Euro Freibetrag):")
+print(f"=> Nötige monatliche Sparrate (konstant) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print(f"=> Nötige monatliche Sparrate (muss jedes Jahr um {angenommene_inflationsrate_in_prozent}% erhöht werden) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print("")
+print(f"(2) Monatliche Rente in Höhe von {monatliche_wunschrente} Euro ({colors.CGREEN}netto{colors.CEND}, progressiver persönlicher Einkommensteuersatz, ledig):")
+print(f"=> Nötige monatliche Sparrate (konstant) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print(f"=> Nötige monatliche Sparrate (muss jedes Jahr um {angenommene_inflationsrate_in_prozent}% erhöht werden) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print("")
+print(f"(3) Monatliche Rente in Höhe von {monatliche_wunschrente} Euro ({colors.CGREEN}netto{colors.CEND}, progressiver persönlicher Einkommensteuersatz, verheiratet):")
+print(f"=> Nötige monatliche Sparrate (konstant) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print(f"=> Nötige monatliche Sparrate (muss jedes Jahr um {angenommene_inflationsrate_in_prozent}% erhöht werden) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+
+# (b) Mit Inflationsausgleich (d.h. steigende Entnahme in der Rentenphase):
+
+print("")
+print(f"(b) Es soll ein Inflationsausgleich in der Rentenphase stattfinden, deine monatlich entnomme Rente soll jedes Jahr um {angenommene_inflationsrate_in_prozent}% als Inflationsausgleich erhöht werden:")
+print("")
+print(f"(0) Monatliche Rente in Höhe von {monatliche_wunschrente} bis {noetige_inflationsangepasste_rente_zum_rentenende} Euro ({colors.CGREEN}brutto{colors.CEND}):")
+
+notwendiges_vermoegen_fuer_msci_verzehrende_monatliche_entnahme_strategie_mit_inflationsausgleich = funktion_invertieren(vermoegens_verzehrung_verzinster_entnahmeplan_steigende_entnahme, dauer_rentenphase_in_jahren, "vermoegen", {"monatliche_entnahme": monatliche_wunschrente, "jaehrliche_steigerung_entnahme": angenommene_inflationsrate_in_prozent, "jaehrliche_rendite_entnahmeplan": msci_world_rendite}, praezision=0.1, initialer_schaetzwert=10000)
+
+print(f"Zum Renteneintritt musst du ein Vermögen von {notwendiges_vermoegen_fuer_msci_verzehrende_monatliche_entnahme_strategie_mit_inflationsausgleich} Euro im MSCI World angespart haben, um danach deine (mit der Inflation mitwachsende) Rente in Höhe von zunächst {monatliche_wunschrente} Euro (brutto) verzehrend entnehmen zu können.")
+
+notwendige_konstante_monatliche_sparrate_fuer_msci_verzehrende_monatliche_entnahme_strategie_mit_inflationsausgleich = (1.0/12.0) * funktion_invertieren(sparplan_ausfuehren, notwendiges_vermoegen_fuer_msci_verzehrende_monatliche_entnahme_strategie_mit_inflationsausgleich, "sparrate_initial", {"sparrate_steigerungsrate": 1.00, "wertsteigerung_anlageobjekt": 1.00 + (msci_world_rendite/100.0), "anlage_dauer": dauer_sparphase_in_jahren})
+notwendige_wachsende_monatliche_sparrate_fuer_msci_verzehrende_monatliche_entnahme_strategie_mit_inflationsausgleich = (1.0/12.0) * funktion_invertieren(sparplan_ausfuehren, notwendiges_vermoegen_fuer_msci_verzehrende_monatliche_entnahme_strategie_mit_inflationsausgleich, "sparrate_initial", {"sparrate_steigerungsrate": 1.00 + (angenommene_inflationsrate_in_prozent/100.0), "wertsteigerung_anlageobjekt": 1.00 + (msci_world_rendite/100.0), "anlage_dauer": dauer_sparphase_in_jahren})
+
+print(f"=> Nötige monatliche Sparrate (konstant) = {colors.CGREEN}{notwendige_konstante_monatliche_sparrate_fuer_msci_verzehrende_monatliche_entnahme_strategie_mit_inflationsausgleich} Euro{colors.CEND}")
+print(f"=> Nötige monatliche Sparrate (muss jedes Jahr um {angenommene_inflationsrate_in_prozent}% erhöht werden) = {colors.CGREEN}{notwendige_wachsende_monatliche_sparrate_fuer_msci_verzehrende_monatliche_entnahme_strategie_mit_inflationsausgleich} Euro{colors.CEND}")
+print("")
+print(f"(1) Monatliche Rente in Höhe von {monatliche_wunschrente} bis {noetige_inflationsangepasste_rente_zum_rentenende} Euro ({colors.CGREEN}netto{colors.CEND}, {kapitalertragssteuer_in_prozent}% Kapitalertragssteuer, {freibetrag_in_euro} Euro Freibetrag):")
+print(f"=> Nötige monatliche Sparrate (konstant) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print(f"=> Nötige monatliche Sparrate (muss jedes Jahr um {angenommene_inflationsrate_in_prozent}% erhöht werden) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print("")
+print(f"(2) Monatliche Rente in Höhe von {monatliche_wunschrente} bis {noetige_inflationsangepasste_rente_zum_rentenende} Euro ({colors.CGREEN}netto{colors.CEND}, progressiver persönlicher Einkommensteuersatz, ledig):")
+print(f"=> Nötige monatliche Sparrate (konstant) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print(f"=> Nötige monatliche Sparrate (muss jedes Jahr um {angenommene_inflationsrate_in_prozent}% erhöht werden) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print("")
+print(f"(3) Monatliche Rente in Höhe von {monatliche_wunschrente} bis {noetige_inflationsangepasste_rente_zum_rentenende} Euro ({colors.CGREEN}netto{colors.CEND}, progressiver persönlicher Einkommensteuersatz, verheiratet):")
+print(f"=> Nötige monatliche Sparrate (konstant) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print(f"=> Nötige monatliche Sparrate (muss jedes Jahr um {angenommene_inflationsrate_in_prozent}% erhöht werden) = {colors.CGREEN}{0} Euro{colors.CEND}") # !!!ToDo!!!
+print("")
 print("=> Vor- und Nachteile:")
 print(f"(+) Dadurch dass das Vermögen verzehrt wird, muss weniger angespart werden.")
+print(f"(+) Anders als bei der Dividenden-Strategie gibt es in der Ansparphase einen echten Zinseszinseffekt.")
 print(f"(-) Das Vermögen wird verzehrt und ist am Ende aufgebraucht.")
 
 # ##### Besparen des MSCI World, zweimalige Entnahme der Rente (einmal zu Rentenbeginn und einmal während der Rente), Verzehrung des Vermögens: #####
 print("")
 print(colors.CGREEN + "##### Besparen des MSCI World, zweimalige Entnahme der Rente (einmal zu Rentenbeginn und einmal während der Rente), Verzehrung des Vermögens: #####" + colors.CEND)
+print("ToDo")
+print("=> Vor- und Nachteile:")
+print(f"(+) Größere Sicherheit bezüglich Kursschwankungen: man kennt seine genaue Rentenhöhe.")
+print(f"(+) Anders als bei der Dividenden-Strategie gibt es in der Ansparphase einen echten Zinseszinseffekt.")
+print(f"(-) Das auf einen Schlag entnommene Vermögen kann sich nicht mehr weiter unter dem Zinseszinseffekt vermehren (in der Rentenphase).")
+print(f"(-) Das Vermögen wird verzehrt und ist am Ende aufgebraucht.")
+
+# ##### Besparen des MSCI World, einmalige Entnahme der Rente (zu Rentenbeginn), Verzehrung des Vermögens: #####
+print("")
+print(colors.CGREEN + "##### Besparen des MSCI World, einmalige Entnahme der Rente (zu Rentenbeginn), Verzehrung des Vermögens: #####" + colors.CEND)
 print("ToDo")
 print("=> Vor- und Nachteile:")
 print(f"(+) Größere Sicherheit bezüglich Kursschwankungen: man kennt seine genaue Rentenhöhe.")
